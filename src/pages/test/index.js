f55e2565829f11c3fbccfb24cc1cc419469d031d
dongyun <ehddbs452100@naver.com>
2025-03-06 10:25:38 +0900

    feat(db): insert answer

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,20 +1,25 @@
 import { checkAnswer } from '../../utils/grading.js';
+import { insertAnswer } from '../../utils/post.user.choice.js';
 
-let answer = [];
+const answer = [];
 
-function nextQuestion(current, checkedNumber) {
-  answer.push(checkedNumber);
-  document.getElementById('q' + current).classList.remove('active');
-  document.getElementById('d' + current).classList.remove('active');
-  document.getElementById('d' + current).classList.add('done');
-  var nextQuestion = document.getElementById('q' + (current + 1));
-  var nextDot = document.getElementById('d' + (current + 1));
-  if (nextQuestion && nextDot) {
-    nextQuestion.classList.add('active');
-    nextDot.classList.add('active');
-  } else {
-    window.location.href = `/src/pages/result/index.html?score=${checkAnswer(answer)}`;
-  }
+async function nextQuestion(current,checkedNumber) {
+    answer.push(checkedNumber);
+    
+    document.getElementById('q' + current).classList.remove('active');
+    document.getElementById('d' + current).classList.remove('active');
+    document.getElementById('d' + current).classList.add('done');
+
+    const nextQuestion = document.getElementById('q' + (current + 1));
+    const nextDot = document.getElementById('d' + (current + 1));
+
+    if (nextQuestion && nextDot) {
+        nextQuestion.classList.add('active');
+        nextDot.classList.add('active');
+    } else {
+        await insertAnswer(answer);
+        window.location.href = `/src/pages/result/index.html?score=${checkAnswer(answer)}`;
+    }
 }
 
 window.nextQuestion = nextQuestion;
